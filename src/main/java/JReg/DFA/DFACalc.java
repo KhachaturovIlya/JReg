82f1f0934d84ab99bcaab38f63544ecdf67ac566
IluhaZapivuha <khachaturov.ilya@yandex.ru>
2026-05-25 23:23:11 +0300

    Add correct automat logic operations

--- src/main/java/JReg/DFA/DFACalc.java
+++ src/main/java/JReg/DFA/DFACalc.java
@@ -2,7 +2,7 @@ package JReg.DFA;
 
 import java.util.*;
 
-// TODO: testing and correct work
+// TODO: testing
 public class DFACalc {
     static public DFA addition(DFA dfa) {
         DFA newDfa = new DFA(dfa);
@@ -40,7 +40,6 @@ public class DFACalc {
 
     static public DFA intersection(DFA first, DFA second) {
         Map<NodePair, Node> visited = new HashMap<>();
-
         DFA newDfa = new DFA();
 
         NodePair nowPair = new NodePair(first.getStartNode(), second.getStartNode());
@@ -50,30 +49,97 @@ public class DFACalc {
         Node startNode = newDfa.addNode();
         visited.put(nowPair, startNode);
 
+        if (first.getFinalNodes().contains(nowPair.first()) &&
+                second.getFinalNodes().contains(nowPair.second())) {
+            newDfa.getFinalNodes().add(startNode);
+        }
+
         while (!queue.isEmpty()) {
             NodePair pair = queue.poll();
             Node node = visited.get(pair);
 
-            for (Edge firstEdge : pair.first.getEdges()) {
-                for (Edge secondEdge : pair.second.getEdges()) {
+            for (Edge firstEdge : pair.first().getEdges()) {
+                for (Edge secondEdge : pair.second().getEdges()) {
                     Set<Character> intersection = new HashSet<>(firstEdge.transitionChars());
                     intersection.retainAll(secondEdge.transitionChars());
 
-                    NodePair newPair = new NodePair(firstEdge.to(), secondEdge.to());
-                    if (!intersection.isEmpty() && !visited.containsKey(newPair)) {
-                        Node newNode = newDfa.addNode();
-                        node.getEdges().add(new Edge(new ArrayList<>(intersection), newNode));
-                        visited.put(newPair, newNode);
-                        queue.add(newPair);
+                    if (!intersection.isEmpty()) {
+                        NodePair newPair = new NodePair(firstEdge.to(), secondEdge.to());
+                        Node targetNode;
+
+                        if (!visited.containsKey(newPair)) {
+                            targetNode = newDfa.addNode();
+                            visited.put(newPair, targetNode);
+                            queue.add(newPair);
+
+                            if (first.getFinalNodes().contains(newPair.first()) &&
+                                    second.getFinalNodes().contains(newPair.second())) {
+                                newDfa.getFinalNodes().add(targetNode);
+                            }
+                        } else {
+                            targetNode = visited.get(newPair);
+                        }
+
+                        node.getEdges().add(new Edge(new ArrayList<>(intersection), targetNode));
                     }
                 }
             }
         }
-        
+
         return newDfa;
     }
 
     static public DFA subtraction(DFA first, DFA second) {
         return intersection(first, addition(second));
+    }
+    
+    public static boolean isEqual(DFA a, DFA b) {
+        Set<NodePair> visited = new HashSet<>();
+        Queue<NodePair> queue = new ArrayDeque<>();
+
+        NodePair start = new NodePair(a.getStartNode(), b.getStartNode());
+        queue.add(start);
+        visited.add(start);
+        
+        Set<Character> alphabet = new HashSet<>(a.getAlphabet());
+        alphabet.addAll(b.getAlphabet());
+
+        while (!queue.isEmpty()) {
+            NodePair pair = queue.poll();
+            
+            boolean isFirstFinal = a.getFinalNodes().contains(pair.first());
+            boolean isSecondFinal = b.getFinalNodes().contains(pair.second());
+            if (isFirstFinal != isSecondFinal) {
+                return false;
+            }
+            
+            for (char c : alphabet) {
+                Node nextA = getTransition(pair.first(), c);
+                Node nextB = getTransition(pair.second(), c);
+
+                if (nextB == null && nextA == null) {
+                    continue;
+                } else if (nextB == null || nextA == null) {
+                    return false;
+                }
+
+                NodePair nextPair = new NodePair(nextA, nextB);
+                if (!visited.contains(nextPair)) {
+                    visited.add(nextPair);
+                    queue.add(nextPair);
+                }
+            }
+        }
+        return true;
+    }
+    
+    private static Node getTransition(Node node, char c) {
+        if (node == null) return null;
+        for (Edge edge : node.getEdges()) {
+            if (edge.transitionChars().contains(c)) {
+                return edge.to();
+            }
+        }
+        return null;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import JReg.AST.LabeledSyntaxTree;
 import JReg.AST.PreparedRegularStatement;
 import JReg.AST.SyntaxTree;
+import JReg.DFA.DFA;
+import JReg.DFA.DFACalc;
 import JReg.DFA.DFAMinimizer;
+import JReg.DFA.DFARenderer;
 import JReg.NFA.NFA;
 import JReg.NFA.NFARenderer;
 
@@ -14,20 +18,26 @@ public class Main {
         File outNFAfile = new File("files/NFA.png");
 
         // DFAMinimizer dfaMinimizer = new DFAMinimizer();
-        String reg = "x(<first>a|b)y";
+        String reg1 = "(a|b)...";
+        String reg2 = "(ab)...";
 
-        // DFA dfa = new DFA(new LabeledSyntaxTree(new SyntaxTree(new PreparedRegularStatement(reg))));
+        DFA dfa1 = new DFA(new LabeledSyntaxTree(new SyntaxTree(new PreparedRegularStatement(reg1))));
+        DFA dfa2 = new DFA(new LabeledSyntaxTree(new SyntaxTree(new PreparedRegularStatement(reg2))));
 
-        /* DFARenderer renderer = new DFARenderer(outFile);
-        renderer.renderDFA(dfa);
+        DFA subDfa = DFACalc.subtraction(dfa1, dfa2);
 
+        DFARenderer renderer = new DFARenderer(outFile);
+        renderer.renderDFA(subDfa);
+        System.out.println(DFACalc.isEqual(DFACalc.addition(DFACalc.addition(dfa1)), dfa2));
+
+        /*
         dfa = dfaMinimizer.minimizeDFA(dfa);
         renderer = new DFARenderer(minOutFile);
         renderer.renderDFA(dfa); */
 
-        NFA nfa = new NFA(new SyntaxTree(new PreparedRegularStatement(reg)));
+        // NFA nfa = new NFA(new SyntaxTree(new PreparedRegularStatement(reg)));
 
-        NFARenderer renderer = new NFARenderer(outNFAfile);
-        renderer.renderNFA(nfa);
+        // NFARenderer renderer = new NFARenderer(outNFAfile);
+        // renderer.renderNFA(nfa);
     }
 }
